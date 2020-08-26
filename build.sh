df90f9bddb3693250ab0f72a718a1f9563c69464
brinkqiang <brink.qiang@gmail.com>
2020-08-26 10:32:01 +0800

    add

--- build.sh
+++ build.sh
@@ -5,22 +5,11 @@
 # yum -y install gcc gcc-c++ autoconf libtool automake make
 #
 
-# - clone code
-# git clone https://github.com/brinkqiang/dmformat.git
-# pushd dmformat
-# git submodule update --init --recursive
-#
-
-# pushd depends_path
-# libtoolize && aclocal && autoheader && autoconf && automake --add-missing
-# sh configure
-# popd
-
 rm -rf build
 mkdir build
 pushd build
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-make -j
+cmake --build . --config relwithdebinfo
 popd
 
 # popd
